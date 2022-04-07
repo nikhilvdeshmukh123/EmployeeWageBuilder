6eceb8821abebb75b8cb38586967e8445b83903a
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-04-07 18:48:13 +0530

    Added: Calculating Employee Wage For Month

--- src/com/bridgelabz/EmployeeWageUC3.java
+++ src/com/bridgelabz/EmployeeWageUC3.java
@@ -4,12 +4,15 @@ public class EmployeeWageUC3 {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
 
 	public static void main(String[] args) {
 		// Variables
 		int empHrs = 0;
 		int empWage = 0;
+		int totalEmpWage = 0;
 		// Computation
+		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
 		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
 		switch (empCheck) {
 			case IS_PART_TIME:
@@ -22,6 +25,9 @@ public class EmployeeWageUC3 {
 				empHrs = 0;
 		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
+		totalEmpWage += empWage;
 		System.out.println("Emp Wage: " + empWage);
+		}
+		System.out.println("Total Employee Wage: " + totalEmpWage);
 	}
 }
