d4ea5176da2d799f5f1633a245c79de0286254f1
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-04-11 00:26:32 +0530

    Added: Refactor code to write class method to compute Emp Wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,21 +1,27 @@
 package com.bridgelabz;
-
+/*
+ * @author: Nikhil
+ * Compute employee wage using class methods
+ */
 public class EmployeeWage {
+	//Constants
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
 	public static final int MAX_HRS_IN_MONTH = 100;
-
-	public static void main(String[] args) {
-		// Variables
+	// class method
+	public static int computeEmpWage() {
+		//Declaring Variables
 		int empHrs = 0;
 		int totalEmpHrs = 0;
 		int totalWorkingDays = 0;
 		// Computation
 		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
 			totalWorkingDays++;
+			//Generating random numbers
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			// Switch case
 			switch (empCheck) {
 			case IS_PART_TIME:
 				empHrs = 4;
@@ -29,7 +35,15 @@ public class EmployeeWage {
 			totalEmpHrs += empHrs;
 			System.out.println("Day: " + totalWorkingDays + " Emp hr: " + empHrs);
 		}
+		// Calculating Employee Wage
 		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Total Employee Wage: " + totalEmpWage);
+		return totalEmpWage;
+	}
+	//main method
+	public static void main(String[] args) {
+		//Displaying welcome message 
+		System.out.println("Welcome to the Employee Wage Computation Programme");
+		computeEmpWage();
 	}
 }
