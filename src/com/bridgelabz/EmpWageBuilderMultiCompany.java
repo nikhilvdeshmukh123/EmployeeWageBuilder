d752d3a9b7427a91bf803db4f3f020a261ab8a94
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-04-15 14:55:04 +0530

    Added: Employee Wage For Multiple Company

--- src/com/bridgelabz/EmpWageBuilderMultiCompany.java
+++ src/com/bridgelabz/EmpWageBuilderMultiCompany.java
@@ -1,25 +1,24 @@
 package com.bridgelabz;
+
 /*
  * @author: Nikhil
- * Compute employee wage using class methods
+ * Compute employee wage for Multiple company in procedural way using class methods
  */
-public class EmployeeWage {
-	//Constants
+public class EmpWageBuilderMultiCompany {
+	// Constants
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
+
 	// class method
-	public static int computeEmpWage() {
-		//Declaring Variables
+	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		// Declaring Variables
 		int empHrs = 0;
 		int totalEmpHrs = 0;
 		int totalWorkingDays = 0;
 		// Computation
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
-			//Generating random numbers
+			// Generating random numbers
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			// Switch case
 			switch (empCheck) {
@@ -36,14 +35,17 @@ public class EmployeeWage {
 			System.out.println("Day: " + totalWorkingDays + " Emp hr: " + empHrs);
 		}
 		// Calculating Employee Wage
-		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Total Employee Wage: " + totalEmpWage);
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("Total Employee Wage for Company " + company + " is: " + totalEmpWage);
 		return totalEmpWage;
 	}
-	//main method
+
+	// main method
 	public static void main(String[] args) {
-		//Displaying welcome message 
+		// Displaying welcome message
 		System.out.println("Welcome to the Employee Wage Computation Programme");
-		computeEmpWage();
+		computeEmpWage("DMart", 20, 20, 100);
+		computeEmpWage("Reliance", 25, 40, 150);
+		computeEmpWage("Tata", 18, 30, 120);
 	}
 }
